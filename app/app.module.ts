70a2ec167852a819d77ce4b6eb8467efd62771f5
mazergio <135391139+mazergio@users.noreply.github.com>
2023-08-08 22:38:42 -0300

    Angular general

---
+++ app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { CuerpoComponent } from './componentes/cuerpo/cuerpo.component';
+import { PiepaginaComponent } from './componentes/piepagina/piepagina.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    CuerpoComponent,
+    PiepaginaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
